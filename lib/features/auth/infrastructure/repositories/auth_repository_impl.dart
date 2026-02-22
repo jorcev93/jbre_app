6a6c75972c026b31da61047a56ef6542b3ccad67
Jorge Cevallos <jorge.cev@outlook.es>
2026-02-22 15:07:36 -0500

    feat: auth-domain

---
+++ lib/features/auth/infrastructure/repositories/auth_repository_impl.dart
@@ -0,0 +1,24 @@
+import 'package:jbre_app/features/auth/domain/domain.dart';
+import 'package:jbre_app/features/auth/infrastructure/infraestructure.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final AuthDataSource dataSource;
+
+  AuthRepositoryImpl(AuthDataSource? dataSource)
+    : dataSource = dataSource ?? AuthDataSourceImpl();
+
+  @override
+  Future<User> checkAuthStatus(String token) {
+    return dataSource.checkAuthStatus(token);
+  }
+
+  @override
+  Future<User> login(String email, String password) {
+    return dataSource.login(email, password);
+  }
+
+  @override
+  Future<User> register(String email, String password, String fullName) {
+    return dataSource.register(email, password, fullName);
+  }
+}
